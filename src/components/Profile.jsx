69e68dfa5519c43a9cad5ad02e8904467e9489cb
Maks <maksym.demeshok@gmail.com>
2020-10-17 21:17:19 +0200

    components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Profile = () => {
+   return <div className='content'>
+      <div>
+         <img src="https://images.pexels.com/photos/239520/pexels-photo-239520.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+      </div>
+      <div>
+         ava+desc
+   </div>
+      <div>
+         my post
+   </div>
+      <div>
+         new post
+   </div>
+      <div>post1</div>
+      <div>post2</div>
+   </div>
+}
+
+
+export default Profile;

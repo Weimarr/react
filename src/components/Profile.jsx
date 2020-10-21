a19ae72d8ddee4f3229a25a93d1ebaaf9de58d77
Maks <maksym.demeshok@gmail.com>
2020-10-21 19:57:28 +0200

    some css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,9 +7,12 @@ const Profile = () => {
       <div>
          <img src="https://images.pexels.com/photos/239520/pexels-photo-239520.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
       </div>
-      <div>
-         ava+desc
-   </div>
+      <div className={classes.profile_data}>
+         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRVWJ6q2QHoeCc_VK-fBB3AZKv9dN2ECoda5Q&usqp=CAU" alt="" className={classes.avatar} />
+         <div className={classes.desc}> Max Demeshok <br/>
+            Frontend Developer
+         </div>
+      </div>
       <div>
          my post
    </div>
